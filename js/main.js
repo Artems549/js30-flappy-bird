3fcf313cd68d48ffb67e2a98deab607438d92201
Artem <sfmillingg@gmail.com>
2023-10-11 13:56:23 +0400

    add: basic layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log(true)
+let score = document.querySelector('.header__score p');
+
+
+
+let gameField = document.getElementById('main__canvas');
+let context = gameField.getContext('2d');
+
+let fieldWidth = gameField.offsetWidth
+let fieldHeight = gameField.offsetHeight
+console.log(fieldWidth)
+console.log(fieldHeight)
+
+context.fillStyle = 'white'
+context.fillRect(0, 0, fieldWidth, fieldHeight)
+
+score.innerHTML = '0'
